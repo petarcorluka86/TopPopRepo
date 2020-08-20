a183a455a274ec12ebd26baa72bd7e4c002725d4
petarcorluka86 <petarcorluka86@gmail.com>
2020-08-20 17:06:18 +0200

    Old code swapped with newer version.

--- Info/TopPop_v2/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ListPage from './components/ListPage'
-
-ReactDOM.render(<ListPage/>,document.getElementById('root'))
--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
-import ListPage from './components/ListPage';
-import {Router, Route, Switch} from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ListPage from './components/ListPage'
 
-const hist = createBrowserHistory()
-
-ReactDOM.render(
-  // eslint-disable-next-line react/jsx-filename-extension
-  <Router history={hist}>
-    <Switch>
-      <Route path="/" component={ListPage} />
-    </Switch>
-  </Router>,
-  document.getElementById('root')
-);
-
-serviceWorker.unregister();
+ReactDOM.render(<ListPage/>,document.getElementById('root'))
